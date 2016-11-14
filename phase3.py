d71a1c56c1f41a34ec2bb77b19d9d23209d9854a
rauten <rauten3@gatech.edu>
2016-11-13 19:34:06 -0500

    Created phase3.py file

---
+++ phase3.py
@@ -0,0 +1,11 @@
+from Tkinter import *
+import re
+import string
+
+class masterGUI:
+
+    def __init__(self, win):
+        self.win = win
+        self.label1 = Label(self.win, text = "Login")
+        self.label1.grid(row = 0, column = 0, columnspan = 2)
+        

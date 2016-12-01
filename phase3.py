ada5dee19e5a60516a001172fa2a51ea0791bbc5
niharullal <niharullal18@gmail.com>
2016-11-30 23:41:44 -0500

    RegisterNew
    Working through Register New function still

--- phase3.py
+++ phase3.py
@@ -39,11 +39,16 @@ class masterGUI:
         self.btLogin = Button(self.win, width = 5, padx=5, pady=5, text = "Login", command = self.LoginCheck)
         self.btLogin.grid(row = 3, column = 1, sticky=W)
 
+    def showWin(self):
+        self.win.deiconify()
+        self.registerpage.withdraw()
 
     def Connect(self):
         try:
-            self.db = pymysql.connect(host="acacdemic-mysql.cc.gatech.edu",passwd="Z8IHtiyg",user="cs4400_Team_9",db="cs4400_Team_9")
+            self.db = pymysql.connect(host="academic-mysql.cc.gatech.edu",passwd="Z8IHtiyg",user="cs4400_Team_9",db="cs4400_Team_9")
+            self.cursor = self.db.cursor()
         except:
+            raise
             messagebox.showinfo("Connection error. Check your Internet Connection and/or code!")
 
 
@@ -78,28 +83,58 @@ class masterGUI:
         self.cp = Entry(self.smallFrame, width=50)
         self.cp.grid(row=4, column=1, sticky=E, pady=5)
 
-        regButton = Button(self.bigFrame, text="Create", width=10, bg="white") #command = self.RegisterNew
+        regButton = Button(self.bigFrame, text="Create", width=10, bg="white",command = self.RegisterNew)
         regButton.grid(row=7, column=0)
 
     
     def RegisterNew(self):
         self.Connect()
-        self.sql = "SELECT Username,Password FROM USER"
+        self.sql = "SELECT Username,GT_Email,Password FROM STUDENT"
 
-        self.cursorA.execute(self.sql)
+        self.cursor.execute(self.sql)
         information = self.cursor.fetchall()
+        self.db.close()
 
+        self.uname = self.usernameReg.get()
+        self.passw = self.passwordReg.get()
+        self.passconf = self.cp.get()
+        self.emailconf = self.email.get()
+        null = "0"
 
+        if self.uname == "" or self.passw == "" or self.passconf == "" or self.emailconf == "":
+            messagebox.showwarning("Error!", "Cannot have empty field")
+        elif self.passconf != self.passw:
+            messagebox.showwarning("Error", "Passwords do not match! Try again.")
+        elif "@gatech.edu" not in self.emailconf:
+            messagebox.showwarning("Error!", "Please enter valid email")
+        else:
+            fruit = True
+            for each in information:
+                if self.emailconf in each:
+                    messagebox.showwarning("Error", "Email already in system! Please try again!")
+                elif self.uname in each:
+                    messagebox.showwarning("Error", "Username already in system. Please try again!")
+                    fruit = False
+                if fruit:
+                    self.Connect()
+                    self.sql2 = "INSERT INTO STUDENT(Username, GT_Email, Password, Year, Major_Name) VALUES(%s,%s,%s,%s,%s)"
+                    self.cursor.execute(self.sql2,(self.uname,self.emailconf,self.passw,null,null))
+                    self.db.commit()
+                    self.db.close()
+                    messagebox.showwarning("Successful Registration!", "Please log in!")
+                    self.showWin()
+                    
+            
     def LoginCheck(self):
-        self.db2 = self.Connect()
+        self.Connect()
 
         self.userLogin = self.usernameEntry.get()
         self.passLogin = self.passwordEntry.get()
 
+        self.sql = "SELECT * FROM USER WHERE Username = %s AND Password = %s"
+
         try:
-            self.cursor = self.db2.cursor()
-            self.sql = "SELECT * FROM USER WHERE Username = %s AND Password = %s"
-            info = self.cursor.execute(self.sql,(self.userLogin, self.passLogin))
+            self.cursor.execute(self.sql,(self.userLogin, self.passLogin))
             info2 = self.cursor.fetchall()
             if len(info2==0):
                 messagebox.showwarning("Error! Data entered not registered username/password combination.")
@@ -121,7 +156,7 @@ class masterGUI:
         self.titleReg = Label(self.smallFrame,text="Main Page", width=30, padx=5, pady=5, fg="blue", font=("Helvetica", 16))
         self.titleReg.grid(row=0, column=1)
         self.usernameReg = Entry(self.smallFrame, width=50)
-
+        
 
 
 
